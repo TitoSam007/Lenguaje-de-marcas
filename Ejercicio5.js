b9010666fcd676b2f53ae60b1b708677416ca1d0
Base de datos <zamorasamuel007@gmail.com>
2023-04-19 12:05:02 +0200

    Primer commit

---
+++ Ejercicio5.js
@@ -0,0 +1,13 @@
+/*7. Diseñar un algoritmo para convertir temperaturas en grados Farenheit a grados Celsius. El
+programa terminará cuando lea una temperatura igual a 999. Como todo el mundo sabe, la
+relación entre grados Farenheit (F) y grados Celsius (C) viene dada por: C= 5/9 (F – 32).
+Compruebe si 50 º Farenheit da 10ºCelsius.*/
+
+<html>
+    <head>
+
+    </head>
+    <body>
+        
+    </body>
+</html>
